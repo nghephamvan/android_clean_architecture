e610117b57f95d1461b12cd318a83ff1b7b81ed5
Aki <pvnghe@tma.com.vn>
2019-03-20 11:17:20 +0700

    Add unit test for presenter and listObserver

---
+++ app/src/test/java/aki/pvnghe/unittestapi/ListObserverTest.kt
@@ -0,0 +1,42 @@
+package aki.pvnghe.unittestapi
+
+import aki.pvnghe.domain.model.User
+import aki.pvnghe.unittestapi.users.UsersListObserver
+import aki.pvnghe.unittestapi.users.UsersPresenter
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(UsersPresenter::class)
+class ListObserverTest {
+    private lateinit var usersListObserver: UsersListObserver
+
+    @Mock
+    lateinit var usersPresenter: UsersPresenter
+
+    @Mock
+    lateinit var throwable: Throwable
+
+    @Before
+    fun setUp() {
+        usersListObserver = UsersListObserver(usersPresenter)
+    }
+
+    @Test
+    fun `should show article list when observable emits onSuccess`() {
+        val list = emptyList<User>()
+        usersListObserver.onSuccess(list)
+        verify(usersPresenter).showUserList(list)
+    }
+
+    @Test
+    fun `should print stack trace when observable emits onError`() {
+        usersListObserver.onError(throwable)
+        verify(throwable).printStackTrace()
+    }
+}
---
+++ app/src/test/java/aki/pvnghe/unittestapi/PresenterTest.kt
@@ -0,0 +1,66 @@
+package aki.pvnghe.unittestapi
+
+import aki.pvnghe.domain.GetUsersListUseCase
+import aki.pvnghe.domain.model.User
+import aki.pvnghe.unittestapi.users.UsersListObserver
+import aki.pvnghe.unittestapi.users.UsersPresenter
+import aki.pvnghe.unittestapi.users.UsersView
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(GetUsersListUseCase::class)
+class PresenterTest {
+    private lateinit var presenter: UsersPresenter
+
+    @Suppress("UNKNOWN")
+    private fun <T> any(type: Class<T>): T {
+        Mockito.any<T>(type)
+        return null as T
+    }
+
+    @Mock
+    lateinit var getUsersListUseCase: GetUsersListUseCase
+
+    @Mock
+    lateinit var view: UsersView
+
+    @Before
+    fun setUp() {
+        presenter = UsersPresenter(getUsersListUseCase)
+        presenter.attachView(view)
+    }
+
+    @Test
+    fun `should init view`() {
+        presenter.initialise()
+        verify(view).initialiseView()
+    }
+
+    @Test
+    fun `should dispose subscription`() {
+        presenter.disposeSubscriptions()
+        verify(getUsersListUseCase).dispose()
+    }
+
+    @Test
+    fun `should execute usecase when initialise is invoked`() {
+        presenter.initialise()
+        verify(getUsersListUseCase).execute(any(UsersListObserver::class.java), anyString(), ArgumentMatchers.isNull())
+    }
+
+    @Test
+    fun `should set articles list to view`() {
+        val list = emptyList<User>()
+        presenter.showUserList(list)
+        verify(view).showUserList(list)
+    }
+}

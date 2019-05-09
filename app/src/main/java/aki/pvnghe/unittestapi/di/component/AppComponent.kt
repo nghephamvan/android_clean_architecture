15a8f91ee7eacd28b7ea5806f42ceb43ef8f4080
paduy <paduy@tma.com.vn>
2019-05-09 09:52:57 +0700

    Remove unused code

--- app/src/main/java/aki/pvnghe/unittestapi/di/component/AppComponent.kt
+++ app/src/main/java/aki/pvnghe/unittestapi/di/component/AppComponent.kt
@@ -1,22 +1,19 @@
 package aki.pvnghe.unittestapi.di.component
 
+import aki.pvnghe.data.repository.module.RepositoryModule
 import aki.pvnghe.data.retrofit.module.RetrofitModule
 import aki.pvnghe.data.service.module.ServiceApiModule
+import aki.pvnghe.domain.usecase.module.UseCaseModule
 import aki.pvnghe.unittestapi.App
 import aki.pvnghe.unittestapi.di.module.AppModule
-import aki.pvnghe.data.repository.module.RepositoryModule
-import aki.pvnghe.domain.usecase.module.UseCaseModule
-import aki.pvnghe.unittestapi.users.module.MainActivityModule
-import aki.pvnghe.unittestapi.users.module.UsersFragmentModule
 import dagger.Component
-import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [AndroidSupportInjectionModule::class, AppModule::class, RetrofitModule::class,
-    ServiceApiModule::class, RepositoryModule::class, UseCaseModule::class, MainActivityModule::class, UsersFragmentModule::class])
+    ServiceApiModule::class, RepositoryModule::class, UseCaseModule::class])
 interface AppComponent : AndroidInjector<App> {
     @Component.Builder
     abstract class Builder : AndroidInjector.Builder<App>()
